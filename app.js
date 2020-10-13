40b09fa7864b98e79e62b598d47943bdee8c6eb7
sam <sammybonfanti@gmail.com>
2020-10-12 21:14:19 -0600

    adding and saving documents into firestore

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const list = document.querySelector('ul')
+const form = document.querySelector('form')
 
 const addRecipe = (recipe) => {
     let time = recipe.created_at.toDate()
@@ -20,3 +21,24 @@ db.collection('recipes').get().then(snapshot => {
 }).catch(err => {
   console.log(err);
 });
+
+// add and save documents
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    // for the created_at value on firebase
+    const now = new Date()
+
+    // create object for firebase
+    const recipe = {
+        title: form.recipe.value, // the recipe is the id on the input field
+        created_at: firebase.firestore.Timestamp.fromDate(now)
+    }
+
+    // save into database
+    db.collection('recipes').add(recipe).then(() => {
+        console.log('recipe added');
+    }).catch((err) => {
+        console.log(err);
+    })
+})
