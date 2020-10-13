21f402bdc622d35e00738c0eee0c5dc4749b23b6
sam <sammybonfanti@gmail.com>
2020-10-12 21:32:08 -0600

    deleting documents from firestore

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const list = document.querySelector('ul')
 const form = document.querySelector('form')
 
-const addRecipe = (recipe) => {
+const addRecipe = (recipe, id) => {
     let time = recipe.created_at.toDate()
     let html = `
-        <li>
+        <li data-id='${id}'>
             <div>${recipe.title}</div>
             <div>created at - ${time}</div>
+            <button class='btn btn-danger btn-sm my-2'>delete</button>
         </li>
     `
     list.innerHTML += html
 }
-
+// get documents from firebase
 db.collection('recipes').get().then(snapshot => {
   // console.log(snapshot);
   snapshot.docs.forEach(doc => {
     console.log(doc.data());
-    addRecipe(doc.data());
+    console.log(doc.id);
+
+    addRecipe(doc.data(), doc.id);
   });
 }).catch(err => {
   console.log(err);
@@ -41,4 +44,15 @@ form.addEventListener('submit', (e) => {
     }).catch((err) => {
         console.log(err);
     })
+})
+
+// deleting data
+list.addEventListener('click', e => {
+    if (e.target.tagName === 'BUTTON') {
+        console.log(e);
+        const id = e.target.parentElement.getAttribute('data-id')
+        db.collection('recipes').doc(id).delete().then(() => {
+            console.log('recipe deleted');
+        })
+    }
 })
